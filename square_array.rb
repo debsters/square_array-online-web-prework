35d22547f2fa067e3b6964d1d19971f2aa2e5c95
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-16 18:57:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,4 @@
 def square_array(array)
-<<<<<<< HEAD
   i = 0
   array.each do |x|
     array[i] = x ** 2
@@ -9,11 +8,9 @@ end
 
 def square_array2(array)
   array.collect {|x| x ** 2}
-=======
   array.each do |i|
     i ** 2
     i += 1
   end
   return array
->>>>>>> 0961b886808119ab46d2688b2f3ff169a058cb4f
 end
